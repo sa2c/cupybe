b65e5292d9e66d2522fea9eeb3615940beb0682f
Michele Mesiti <michele.mesiti@gmail.com>
2020-03-27 14:47:06 +0000

    Removed '&&'s in runtests.sh

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,12 +5,12 @@ function run_test(){
     ($@ &> $1.output || (echo "$1 failed." ; exit 1)) && echo "$1 ok."
 }
 
-run_test ./test_calltree1.py profile.cubex && \
-run_test ./test_calltree2.py profile.cubex && \
-run_test ./test_process_multi.py scalasca_output && \
-run_test ./test_convert_df_to_inclusive.py profile.cubex && \
-run_test ./test_convert_df_to_inclusive_multi.py scalasca_output/*/profile.cubex && \
-run_test ./test_convert_index.py scalasca_output/*/profile.cubex && \
-run_test ./test_tree_parsing.py profile.cubex && \
-echo "Testing build of docs" && \
+run_test ./test_calltree1.py profile.cubex 
+run_test ./test_calltree2.py profile.cubex 
+run_test ./test_process_multi.py scalasca_output 
+run_test ./test_convert_df_to_inclusive.py profile.cubex 
+run_test ./test_convert_df_to_inclusive_multi.py scalasca_output/*/profile.cubex
+run_test ./test_convert_index.py scalasca_output/*/profile.cubex
+run_test ./test_tree_parsing.py profile.cubex 
+echo "Testing build of docs" 
 cd ../docs && make html
