8ee90133bc69d56ab2586af9b009d3f647dcca72
Chenna <thechenna123@gmail.com>
2020-04-20 15:02:34 +0100

    added python scripts for plotting

--- plotting/barchartCpathID.py
+++ plotting/barchartCpathID.py
@@ -29,12 +29,13 @@ import matplotlib.pyplot as plt
 import calltree as ct
 import sys
 import os
+import numpy as np
 
 
 inpfilename ="../test_data/profile-25m-nproc40-nsteps10.cubex"
 metric = "time"
 exclincl = False
-nfuncs = 10
+callpathid = 164
 
 if len(sys.argv) > 1:
     inpfilename = sys.argv[1]
@@ -49,21 +50,7 @@ if len(sys.argv) > 3:
     exclincl = (sys.argv[3] == "T")
 
 if len(sys.argv) > 4:
-    nfuncs = int(sys.argv[4])
-
-
-#### get depth level for each function in the call tree
-#
-#######################################################
-
-call_tree = ct.get_call_tree(inpfilename)
-
-df = ct.calltree_to_df(call_tree).set_index('Cnode ID')
-
-parent_series = df['Parent Cnode ID']
-
-levels = ct.get_level(parent_series)
-
+    callpathid = int(sys.argv[4])
 
 #### get the data for the given metric
 #
@@ -79,40 +66,34 @@ output_i = mg.process_cubex(inpfilename, exclusive=exclincl)
 # We convert the Cnode IDs to short callpaths in the dataframe.
 df_i = ic.convert_index(output_i.df, output_i.ctree_df, target = 'Short Callpath')
 
-#df_i = df_i.rename(columns={"Short Callpath": "Short_Callpath"})
-#df_i[['Callpath','CpathID']] = df_i.Short_Callpath.str.split(",",expand=True)
-#df_i.CpathID = df_i.CpathID.astype(int)
+df_i = df_i.reset_index()
+df_i = df_i.rename(columns={"Short Callpath": "Short_Callpath"})
+df_i = df_i.rename(columns={"Thread ID": "Thread_ID"})
+df_i[['Callpath','CpathID']] = df_i.Short_Callpath.str.split(",",expand=True)
+df_i.CpathID = df_i.CpathID.astype(int)
+df_i["Thread_ID"]  = df_i["Thread_ID"].astype(int)
 
 # extract the data
-res = df_i.reset_index()[['Short Callpath', 'Thread ID', metric]].groupby('Short Callpath').sum().sort_values([metric])[metric]
+#res = df_i.reset_index()[['Short Callpath', 'Thread ID', metric]].groupby('Short Callpath').sum().sort_values([metric])[metric]
 
-res_df = pd.DataFrame(res)
+res_df = df_i[df_i['CpathID'] == callpathid]
 
-#res_df.index = pd.MultiIndex.from_tuples(res_df.index.str.split(',').tolist())
-time = res_df.reset_index()['time']
-fname = res_df.reset_index()['Short Callpath'].str.extract(r'(\w+),([0-9]+)')[0]
-cnode_id = res_df.reset_index()['Short Callpath'].str.extract(r'(\w+),([0-9]+)')[1].astype(int)
+#df_i.groupby('Thread_ID').sum()
 
-combined = pd.merge(left= pd.concat([time,fname,cnode_id], axis = 'columns').rename({'time':'time',0:'fname',1:'Cnode ID'},axis = 'columns'), right = levels.reset_index().rename({0 : 'level'}, axis = 'columns'),on = 'Cnode ID')
+print(res_df)
 
-#time_data = combined.sort_values(by = ['level','time']).head(10)
+#res_df.plot(kind='bar', x='Thread_ID', y='time', label=res_df.iloc[0,0])
 
-# to extract levels below third levels
-time_data = combined[combined['level'] == 2].sort_values(by = ['level','time'], ascending=False)
+plt.bar(res_df['Thread_ID'], res_df['time'], label=res_df.iloc[0,0])
 
-print(time_data)
-
-time_data.plot(kind='bar', x='fname', y='time')
-
-plt.xlabel("Function name", fontsize=14)
+plt.xlabel("CPU number", fontsize=14)
 plt.title("metric="+metric+" "+("(Exclusive)" if exclincl==True else "(Inclusive)"), fontsize=14)
-plt.legend('',frameon=False)
+plt.legend()
 
 metric_time_list = ["time", "max_time", "min_time"]
 
 if (metric in metric_time_list):
     plt.ylabel("Time [s]", fontsize=14)
-    plt.yscale('log')
 elif (metric == "visits"):
     plt.ylabel("Number of visits", fontsize=14)
 #    plt.yscale('log')
@@ -121,6 +102,11 @@ else:
     print("Metric type not supported!")
     sys.exit()
 
+print(len(res_df))
+
+xtks = np.linspace(1, len(res_df), num=9, dtype=int)
+print(xtks)
+plt.xticks(xtks)
 plt.xticks(rotation=70)
 plt.tight_layout()
 #plt.show()
